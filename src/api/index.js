4accbe773942823b8e3c5995f77838e06e5c7dd8
Viet Pham <56182628+pxviet1997@users.noreply.github.com>
2021-08-19 11:52:20 +1000

    added reqGetTalent

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,4 @@ const BASE_URL = 'http://localhost:5000';
 export const reqSignIn = (email, password) => ajax(`${BASE_URL}/login`, { email, password });
 export const reqSignUp = (email, password, firstName, lastName, userType) =>
   ajax(`${BASE_URL}/signup`, { email, password, firstName, lastName, userType });
+export const reqGetTalent = (_id) => ajax(`${BASE_URL}/talent`, { _id });
