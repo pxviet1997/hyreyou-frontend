880952f66c161801f265aad1a790307c59119840
Bryany2400 <bryany2400@hotmail.com>
2021-08-22 20:17:50 +1000

    SignIn and Forgot Password layout

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,17 @@ import ajax from './ajax';
 const BASE_URL = 'http://localhost:5000';
 
 export const reqSignIn = (email, password) => ajax(`${BASE_URL}/auth/login`, { email, password }, 'POST');
+
 export const reqSignUp = (email, password, firstName, lastName, userType, mobileNumber) => ajax(`${BASE_URL}/auth/signup`, {
   email, password, firstName, lastName, userType, mobileNumber
 }, 'POST');
+
+export const reqReset = (email) => ajax(`${BASE_URL}/auth /reset`, { email }, 'POST');
+
+//reqForgotPassword
+
+
+//reqEMailChecking
+
+
 export const reqGetTalent = (_id) => ajax(`${BASE_URL}/talent`, { _id });
